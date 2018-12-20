281e5ba82cfc35a2649848dfe909a8e932de74f8
Spencer McCoubrey <smccoubrey@verto.ca>
2018-12-20 01:45:18 -0500

    attempting to deploy angular to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const forceSSL = function() {
   }
 };
 // Instruct the app to use the forceSSL middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 
 const path = require('path');
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
