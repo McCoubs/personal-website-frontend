2419c630d3907f342568d585c40c556175e80c5b
Spencer McCoubrey <smccoubrey@verto.ca>
2018-12-20 00:25:20 -0500

    attempting to deploy angular to heroku

---
+++ frontend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+// redirect all requests to https
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+// Instruct the app to use the forceSSL middleware
+app.use(forceSSL());
+
+const path = require('path');
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
