9371e90fa76dc23778ed939bb6d57afdf1200548
Spencer McCoubrey <smccoubrey@verto.ca>
2018-12-26 22:02:55 -0500

    updated home page and rpoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
   { path: '',
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'project-euler',
     loadChildren: './project-euler/project-euler.module#ProjectEulerModule'
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent
   },
   {
     path: '**',
