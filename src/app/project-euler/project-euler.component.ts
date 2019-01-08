222af5f70616459684789e73309608061450a48d
Spencer McCoubrey <smccoubrey@verto.ca>
2019-01-08 11:25:55 -0500

    added random choice from start

--- src/app/project-euler/project-euler.component.ts
+++ src/app/project-euler/project-euler.component.ts
@@ -10,7 +10,7 @@ import { AdDirective } from '../ad.directive';
 export class ProjectEulerComponent implements OnInit {
 
   sideBarOpen = true;
-  currentProject: string;
+  currentProject = {};
   projectsList: Array<Object>;
   @ViewChild(AdDirective) adHost: AdDirective;
 
@@ -18,6 +18,7 @@ export class ProjectEulerComponent implements OnInit {
 
   ngOnInit() {
     this.projectsList = this.projectEulerService.getProjectsList();
+    this.selectProject(this.projectsList[Math.floor(Math.random() * this.projectsList.length)]);
   }
 
   sideBarToggle() {
@@ -26,8 +27,8 @@ export class ProjectEulerComponent implements OnInit {
 
   selectProject(project) {
     this.sideBarToggle();
-    if (this.currentProject !== project['name']) {
-      this.currentProject = project['name'];
+    if (this.currentProject['name'] !== project['name']) {
+      this.currentProject = project;
       // dynamically insert component into directive
       let componentFactory = this.componentFactoryResolver.resolveComponentFactory(project['component']);
       let viewContainerRef = this.adHost.viewContainerRef;
