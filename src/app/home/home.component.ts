004f395f7f990f363da3e7f9e45bbc9fd8b1449d
Spencer McCoubrey <spencemcc@gmail.com>
2020-07-12 15:56:46 -0400

    home page update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faAngular, faReact, faNodeJs, faJsSquare, faCss3Alt, faHtml5, faPython, faJava, faAccessibleIcon, faDocker } from '@fortawesome/free-brands-svg-icons';
+import { faLaptopCode, faDatabase, faSyncAlt, faGamepad, faBookReader, faTv, faUsers, faTerminal } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  faAngular = faAngular;
+  faReact = faReact;
+  faNodeJs = faNodeJs;
+  faJsSquare = faJsSquare;
+  faCss3Alt = faCss3Alt;
+  faHtml5 = faHtml5;
+  faPython = faPython;
+  faJava = faJava;
+  faAccessibleIcon = faAccessibleIcon;
+  faLaptopCode = faLaptopCode;
+  faDatabase = faDatabase;
+  faSyncAlt = faSyncAlt;
+  faDocker = faDocker;
+  faGamepad = faGamepad;
+  faBookReader = faBookReader;
+  faTv = faTv;
+  faUsers = faUsers;
+  faTerminal = faTerminal;
 
   constructor() {}
 }
